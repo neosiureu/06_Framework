6cafcea11ba5863c8348d4233d78ef98df9cadcb
dudals3530 <dudals3530@naver.com>
2025-04-16 17:47:33 +0900

    스부 db설정까지 함

--- demoProject2/src/main/java/edu/kh/demo/controller/ExampleController.java
+++ demoProject2/src/main/java/edu/kh/demo/controller/ExampleController.java
@@ -6,8 +6,11 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import edu.kh.demo.model.dto.Student;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 
@@ -51,12 +54,91 @@ public class ExampleController {
 		
 		model.addAttribute("fruitList",fruitList);
 		
+		//DTO 객체 model을 이용해서 html로 전달
+		Student std = new Student();
+		
+		std.setStudentNo("12345");
+		std.setName("홍길동");
+		std.setAge(20);
+		
+		model.addAttribute("std",std); 
+		
+		//List<Student> 객체 Model 을 이용해서 html로 전달
+		List<Student> stdList = new ArrayList<>();
+		
+		stdList.add(new Student("1111","김일번",20));
+		stdList.add(new Student("2222","촤이번",30));
+		stdList.add(new Student("3333","홍삼번",25));
+		
+		model.addAttribute("stdList",stdList);                           
+		
+		
+		
+		
+		
+		
 		//src/main/resources/templates/example/ex1.html 로 forward
 		return "example/ex1";
 	}
 	
 	
+	@PostMapping("ex2") // /example/ex2 Post 방식 요청 매핑
+	public String ex2(Model model) {
+		
+		model.addAttribute("str","<h1>테스트중 &times;</h1>");
+		
+		return"example/ex2";
+	}
 	
+	@GetMapping("ex3")
+	public String ex3(Model model) {
+		
+		model.addAttribute("key","제목");
+		model.addAttribute("query","검색어");
+		model.addAttribute("boardNo",10);
+		
+		
+		return"example/ex3";
+	}
 	
+	@GetMapping("ex3/{path}")
+	public String pathVariableTest(@PathVariable("path") int path ) {
+     
+	//controller에서 해야하는일 동일한 경우에
+	 // exmaple/ex3/1 , example/ex3/2 , example/ex3/3 ..
+	 // 주소중 {path} 부분의 값을 가져와서 매개변수로 저장
+	 // Controller 단의 메서드에서 사용할 수 있또록 해줌
+	 // + request scope 자동 세팅
+		
+				log.debug("path : " + path);
+			
+				return "example/testResult";
+	}
+	
+	@GetMapping("ex4")
+	public String pathVariableTest(Model model) {
+		
+		Student std = new Student("6879","잠만보",22);
+		
+		model.addAttribute("std",std);
+		
+		model.addAttribute("num",300);
+		
+		
+		return"example/ex4";
+	}
+	
+	@GetMapping("ex5")
+	public String ex5(Model model) {
+		
+		model.addAttribute("message","타임리프+ javaScript 사용연습");
+		model.addAttribute("num1",12345);
+		
+		Student std = new Student();
+		std.setStudentNo("2222");
+		model.addAttribute("std",std);
+		
+		return"example/ex5";
+	}
 	
 }
---
+++ demoProject2/src/main/java/edu/kh/demo/model/dto/Student.java
@@ -0,0 +1,21 @@
+package edu.kh.demo.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// Spring EL 같은 경우 DTO 객체 출력할 때 getter 가 필수 작성 되어있어야함.
+// -> ${Student.name} == ${Student.getName()}
+
+@NoArgsConstructor
+@Data     // Getter + Setter + ToString
+@AllArgsConstructor //모든 필드 초기화용 매개변수 생성자
+public class Student {
+	
+	private String studentNo ; //학생번호
+	private String name;       // 이름
+	private int    age;        // 나이
+}
+
+
+
